c37030850617142e9b73eff6cbfb558e5e585d2e
MRC@str0 <lipe-castropereira@hotmail.com>
2023-08-07 19:45:54 -0300

    Ultimo Commit
    Dei uma organizada no código, pra deixa-lo mais organizado!

--- Filtro_Vinil/Filtro_Vinil.py
+++ Filtro_Vinil/Filtro_Vinil.py
@@ -1,43 +1,50 @@
 import cv2
 
-img = cv2.imread('img_teste1.jpg')
-img_copy = img.copy()
+class Filtro_Vinil():
 
-def mouse_location(event, x, y, flag, param):
-    global img_copy
+    def __init__(self, img_path):
+        self.img = cv2.imread(img_path)
+        self.img_copy = self.img.copy()
 
-    if event == cv2.EVENT_LBUTTONDOWN:
-        img_copy = apl_efeito((x, y), img)
+        cv2.namedWindow('Filtro Vinil')
+        cv2.setMouseCallback('Filtro Vinil', self.mouse_location)
 
-
-def apl_efeito(center, img):
-    a, l = img.shape[:2]
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    for r in range(0, (l + a), 4):
-        cv2.circle(img_gray, center, r, (0, 0, 0), 1, lineType = cv2.LINE_AA)
-
-    return img_gray
+    
+    def mouse_location(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.draw_circles(self.img, (x, y))
 
 
-cv2.namedWindow('Efeito')
-cv2.setMouseCallback('Efeito', mouse_location)
+    def draw_circles(self, img, center):
+        a, l = img.shape[:2]
+        self.img_copy = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-while True:
-    k = cv2.waitKey(1)
+        for r in range(0, (a + l), 4):
+            cv2.circle(self.img_copy, center, r, (0,0,0), 1, cv2.LINE_AA)
 
-    if k == 27:
-        break
 
-    elif k == ord('s'):
-        name_img = str(input('Nome do Arquivo: '))
-        try:
-            cv2.imwrite(name_img + '.jpg', img_copy)
-        except:
-            print('Erro ao salvar imagem!')
-        else:
-            print('Imagem salva com sucesso!')
-            break
+    def show_image(self):
+        while True:
+            k = cv2.waitKey(1)
 
-    else:
-        cv2.imshow('Efeito', img_copy)
+            if k == 27:
+                break
+
+            elif k == ord('s'):
+                try:
+                    path = str(input('Digite o caminho: '))
+                    cv2.imwrite(path + '.jpg', self.img_copy)
+                
+                except:
+                    print('Erro ao salvar imagem!')
+
+                else:
+                    print('Imagem salva com sucesso!')
+                    break
+
+            else:
+                cv2.imshow('Filtro Vinil', self.img_copy)
+
+
+fv = Filtro_Vinil(r'Filtro_Vinil\img_teste2.jpg')
+fv.show_image()
